74b7a3e4f7769eca14f0994d1574ee11445902ae
JuyelRana <mjuyelrana@gmail.com>
2021-07-08 14:28:58 +0600

    OG:Image Updated

--- pages/_document.js
+++ pages/_document.js
@@ -25,7 +25,7 @@ class MyDocument extends Document {
 
                     <meta property="og:url" content="https://mjuyelrana.com/"/>
                     <meta property="og:type" content="website"/>
-                    <meta property="og:image" content="/img/profile_img.png"/>
+                    <meta property="og:image" content="https://mjuyelrana.com/img/profile_img.png"/>
                     <meta property="og:site_name" content="Md. Juyel Rana | Personal Portfolio"/>
                     <meta property="og:description"
                           content="Very passionate about learning new technologies, breaking down complex problems, and learning from other developers and He always keep himself updated with the latest platforms, frameworks, libraries, and technologies. React Js (Next Js, Redux, Flux, Material UI, React-bootstrap), Vue Js(Nuxt Js, Vuex), PHP (Laravel, Codeigniter), Node Js (Express Js), Native (Java, Android, Swift), Cross Platform (React Native, Ionic), MySQL, MongoDB, Firebase, Digital Ocean,AWS, Cloud, Linux (Apache, Mysql Server), Docker, Kubernetes, Jenkins, Git, Bitbucket, Github, Gitlab, Trello, Jira, ClickUp."/>
