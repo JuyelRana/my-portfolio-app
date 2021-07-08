dcf0bafce5a38ab1508028c53e5a9275df3e941e
JuyelRana <mjuyelrana@gmail.com>
2021-07-08 15:50:03 +0600

    Meta image fixed

--- pages/_document.js
+++ pages/_document.js
@@ -26,15 +26,14 @@ class MyDocument extends Document {
                     <meta property="og:url" content="https://mjuyelrana.com/"/>
                     <meta property="og:type" content="website"/>
                     <meta property="og:image" content="https://mjuyelrana.com/img/profile_img.png"/>
+                    <meta property="og:image:width" content="600"/>
+                    <meta property="og:image:height" content="600"/>
                     <meta property="og:site_name" content="Md. Juyel Rana | Personal Portfolio"/>
                     <meta property="og:description"
                           content="Very passionate about learning new technologies, breaking down complex problems, and learning from other developers and He always keep himself updated with the latest platforms, frameworks, libraries, and technologies. React Js (Next Js, Redux, Flux, Material UI, React-bootstrap), Vue Js(Nuxt Js, Vuex), PHP (Laravel, Codeigniter), Node Js (Express Js), Native (Java, Android, Swift), Cross Platform (React Native, Ionic), MySQL, MongoDB, Firebase, Digital Ocean,AWS, Cloud, Linux (Apache, Mysql Server), Docker, Kubernetes, Jenkins, Git, Bitbucket, Github, Gitlab, Trello, Jira, ClickUp."/>
 
-                    <meta property="og:image:width" content="600"/>
-                    <meta property="og:image:height" content="600"/>
-
-                    <link href="/img/favicon.png" rel="icon"/>
-                    <link href="/img/apple-touch-icon.png" rel="apple-touch-icon"/>
+                    <link href="/img/profile_img.png" rel="icon"/>
+                    <link href="/img/profile_img.png" rel="apple-touch-icon"/>
 
                     <link
                         href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
